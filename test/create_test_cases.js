3cfdbe3f742cc10e281983531d84338ff7e88f36
Randall Schmidt <randallschmidt1@gmail.com>
2019-07-29 14:05:55 +0700

    add tests

---
+++ test/create_test_cases.js
@@ -0,0 +1,43 @@
+const jishoApi = require('./../index.js');
+const path = require('path');
+const fs = require('fs').promises;
+
+const jisho = new jishoApi();
+
+async function createTestCases(outputDir, queries, searchFuncName) {
+  for (let i = 0; i < queries.length; i += 1) {
+    const query = queries[i];
+    const result = await jisho[searchFuncName](query);
+    const testCaseStr = JSON.stringify({ query, expectedResult: result }, null, 2);
+    
+    await fs.writeFile(path.join(outputDir, `${i}.json`), testCaseStr);
+  }
+}
+
+function createKanjiTestCases() {
+  const queries = ['車', '家', '楽', '極上', '贄', 'ネガティブ', 'wegmwrlgkrgmg'];
+  const outputDir = path.join(__dirname, 'kanji_test_cases');
+  return createTestCases(outputDir, queries, 'searchForKanji');
+}
+
+async function createExampleTestCases() {
+  const queries = ['車', '日本人', '彼＊叩く', '皆', 'ネガティブ', 'grlgmregmneriireg'];
+  const outputDir = path.join(__dirname, 'example_test_cases');
+  return createTestCases(outputDir, queries, 'searchForExamples');
+}
+
+async function createPhraseScrapeTestCases() {
+  const queries = ['車', '日本人', '皆', 'ネガティブ', 'grlgmregmneriireg'];
+  const outputDir = path.join(__dirname, 'phrase_scrape_test_cases');
+  return createTestCases(outputDir, queries, 'scrapeForPhrase');
+}
+
+function go() {
+  return Promise.all([
+    createKanjiTestCases(),
+    createExampleTestCases(),
+    createPhraseScrapeTestCases(),
+  ]);
+}
+
+go().catch(err => console.warn(err));
---
+++ test/run_tests.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+const JishoApi = require('../index.js');
+const assert = require('chai').assert;
+
+const jishoApi = new JishoApi();
+
+function getFilePaths(dirname) {
+  const filenames = fs.readdirSync(path.join(__dirname, dirname));
+  return filenames.map(filename => path.join(__dirname, dirname, filename));
+}
+
+describe('Matches previously retrieved results', function() {
+  describe('Kanji test cases', function() {
+    const testCaseFiles = getFilePaths('kanji_test_cases');
+    testCaseFiles.forEach((filePath) => {
+      const testCase = require(filePath);
+      it(`Matches expected response for ${testCase.query}.`, async function() {
+        const result = await jishoApi.searchForKanji(testCase.query);
+        assert.equal(JSON.stringify(result, null, 2), JSON.stringify(testCase.expectedResult, null, 2));
+      }).timeout(10000);
+    });
+  });
+
+  describe('Example test cases', function() {
+    const testCaseFiles = getFilePaths('example_test_cases');
+    testCaseFiles.forEach((filePath) => {
+      const testCase = require(filePath);
+      it(`Matches expected response for ${testCase.query}.`, async function() {
+        const result = await jishoApi.searchForExamples(testCase.query);
+        assert.equal(JSON.stringify(result, null, 2), JSON.stringify(testCase.expectedResult, null, 2));
+      }).timeout(10000);
+    });
+  });
+
+  describe('Phrase scrape test cases', function() {
+    const testCaseFiles = getFilePaths('phrase_scrape_test_cases');
+    testCaseFiles.forEach((filePath) => {
+      const testCase = require(filePath);
+      it(`Matches expected response for ${testCase.query}.`, async function() {
+        const result = await jishoApi.scrapeForPhrase(testCase.query);
+        assert.equal(JSON.stringify(result, null, 2), JSON.stringify(testCase.expectedResult, null, 2));
+      }).timeout(10000);
+    });
+  });
+});
