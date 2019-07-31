2a6e9f6f72cf5c0c07901fdb206ba6f6b1e318e3
Randall Schmidt <randallschmidt1@gmail.com>
2019-07-31 10:15:00 +0700

    unify some test code, use more appropriate assertion type (deepEqual)

--- test/run_tests.js
+++ test/run_tests.js
@@ -10,38 +10,30 @@ function getFilePaths(dirname) {
   return filenames.map(filename => path.join(__dirname, dirname, filename));
 }
 
+function runTestCases(testCaseFiles, apiFunction) {
+  testCaseFiles.forEach((filePath) => {
+    const testCase = require(filePath);
+    it(`Matches expected response for ${testCase.query}.`, async function() {
+      const result = await jishoApi[apiFunction](testCase.query);
+      assert.deepEqual(JSON.parse(JSON.stringify(result, null, 2)), testCase.expectedResult);
+    }).timeout(10000);
+  });
+}
+
 describe('Matches previously scraped results', function() {
   describe('Kanji test cases', function() {
     const testCaseFiles = getFilePaths('kanji_test_cases');
-    testCaseFiles.forEach((filePath) => {
-      const testCase = require(filePath);
-      it(`Matches expected response for ${testCase.query}.`, async function() {
-        const result = await jishoApi.searchForKanji(testCase.query);
-        assert.equal(JSON.stringify(result, null, 2), JSON.stringify(testCase.expectedResult, null, 2));
-      }).timeout(10000);
-    });
+    runTestCases(testCaseFiles, 'searchForKanji');
   });
 
   describe('Example test cases', function() {
     const testCaseFiles = getFilePaths('example_test_cases');
-    testCaseFiles.forEach((filePath) => {
-      const testCase = require(filePath);
-      it(`Matches expected response for ${testCase.query}.`, async function() {
-        const result = await jishoApi.searchForExamples(testCase.query);
-        assert.equal(JSON.stringify(result, null, 2), JSON.stringify(testCase.expectedResult, null, 2));
-      }).timeout(10000);
-    });
+    runTestCases(testCaseFiles, 'searchForExamples');
   });
 
   describe('Phrase scrape test cases', function() {
     const testCaseFiles = getFilePaths('phrase_scrape_test_cases');
-    testCaseFiles.forEach((filePath) => {
-      const testCase = require(filePath);
-      it(`Matches expected response for ${testCase.query}.`, async function() {
-        const result = await jishoApi.scrapeForPhrase(testCase.query);
-        assert.equal(JSON.stringify(result, null, 2), JSON.stringify(testCase.expectedResult, null, 2));
-      }).timeout(10000);
-    });
+    runTestCases(testCaseFiles, 'scrapeForPhrase');
   });
 });
 
