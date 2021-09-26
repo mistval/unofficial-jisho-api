c198af266bae7a4452af0e0e43318be08f6c4bc7
Randall Schmidt <randallschmidt1@gmail.com>
2021-09-26 10:11:33 -0400

    regen test cases

--- test/create_test_cases.js
+++ test/create_test_cases.js
@@ -1,6 +1,10 @@
 import JishoAPI from './../index.js';
 import path from 'path';
 import fs from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const jisho = new JishoAPI();
 
